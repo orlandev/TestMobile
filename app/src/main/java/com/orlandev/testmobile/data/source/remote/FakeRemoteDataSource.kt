ca50c27f4fbe657b377cc79e7e2fef506454265d
Orlando N. Rodriguez <onovas05@gmail.com>
2022-05-21 10:01:11 -0400

    Add Dagger module and Tests for FakeRemoteDataSource.kt

--- app/src/main/java/com/orlandev/testmobile/data/source/remote/FakeRemoteDataSource.kt
+++ app/src/main/java/com/orlandev/testmobile/data/source/remote/FakeRemoteDataSource.kt
@@ -1,11 +1,10 @@
-package com.orlandev.testmobile.data.remote
+package com.orlandev.testmobile.data.source.remote
 
 import com.orlandev.testmobile.domain.api.ApiService
 import com.orlandev.testmobile.domain.model.Product
 import com.orlandev.testmobile.domain.source.DataSource
-import javax.inject.Inject
 
-class FakeRemoteDataSource @Inject constructor(private val apiService: ApiService) : DataSource {
+class FakeRemoteDataSource (private val apiService: ApiService) : DataSource {
     override suspend fun getProductList(): List<Product> {
         return apiService.getProductList()
     }
--- app/src/main/java/com/orlandev/testmobile/di/AppModule.kt
+++ app/src/main/java/com/orlandev/testmobile/di/AppModule.kt
@@ -1,7 +1,11 @@
 package com.orlandev.testmobile.di
 
 import com.orlandev.testmobile.data.api.FakeApiService
+import com.orlandev.testmobile.data.repositories.RemoteRepository
+import com.orlandev.testmobile.data.source.remote.FakeRemoteDataSource
 import com.orlandev.testmobile.domain.api.ApiService
+import com.orlandev.testmobile.domain.repository.DataRepository
+import com.orlandev.testmobile.domain.source.DataSource
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,6 +20,16 @@ object AppModule {
     @Singleton
     fun provideApiService(): ApiService {
         return FakeApiService()
+    }
+
+    @Provides
+    fun provideRemoteDataSource(apiService: ApiService): DataSource {
+        return FakeRemoteDataSource(apiService = apiService)
+    }
+
+    @Provides
+    fun provideRemoteRepository(dataSource: FakeRemoteDataSource): DataRepository {
+        return RemoteRepository(dataSource)
     }
 
 }
