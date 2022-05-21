b9d80ff057edac0083fdbbe9e5f2f0ee172de997
Orlando N. Rodriguez <onovas05@gmail.com>
2022-05-21 11:35:45 -0400

    Add description to the product model

--- app/src/main/java/com/orlandev/testmobile/domain/model/Product.kt
+++ app/src/main/java/com/orlandev/testmobile/domain/model/Product.kt
@@ -2,6 +2,7 @@ package com.orlandev.testmobile.domain.model
 
 data class Product(
     val name: String,
+    val description:String,
     val thumbnailUrl: String,
 )
 
--- app/src/main/java/com/orlandev/testmobile/utils/Utils.kt
+++ app/src/main/java/com/orlandev/testmobile/utils/Utils.kt
@@ -9,6 +9,7 @@ fun generateFakeProducts(): List<Product> {
         list.add(
             Product(
                 name = "ProductName",
+                description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
                 thumbnailUrl = "https://source.unsplash.com/random/300×300"
             )
         )
