46686bec638b787b9780ec68d68f046066ed11d2
Orlando N. Rodriguez <onovas05@gmail.com>
2022-05-24 23:32:27 -0400

    FIXED some UI Issues

--- app/src/main/java/com/orlandev/testmobile/ui/screens/login/LoginScreen.kt
+++ app/src/main/java/com/orlandev/testmobile/ui/screens/login/LoginScreen.kt
@@ -1,9 +1,12 @@
 package com.orlandev.testmobile.ui.screens.login
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Key
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.runtime.Composable
@@ -11,6 +14,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
@@ -45,125 +49,150 @@ fun LoginScreen(
 
 
 
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+    Box(modifier = Modifier.fillMaxSize()) {
 
-        Column(
+        Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(20.dp),
-            verticalArrangement = Arrangement.SpaceAround,
+                .fillMaxWidth()
+                .height(300.dp)
+                .background(MaterialTheme.colors.primary)
+        )
 
-            ) {
+        Column(modifier = Modifier) {
             Text(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(horizontal = 20.dp),
+                    .padding(top = 20.dp)
+                    .weight(4f),
                 text = stringResource(id = R.string.app_name),
-                style = MaterialTheme.typography.h5,
-                textAlign = TextAlign.Center
+                style = MaterialTheme.typography.h4,
+                textAlign = TextAlign.Center,
+                color = Color.White
             )
-
-            Column(
-                modifier = Modifier,
-                verticalArrangement = Arrangement.spacedBy(12.dp)
+            Card(
+                elevation = 10.dp,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp)
+                    .weight(18f)
             ) {
-                Text(
-                    text = stringResource(id = R.string.login_with_simple_account),
-                    style = MaterialTheme.typography.caption
-                )
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    label = { Text(text = stringResource(id = R.string.user_name_text)) },
-                    value = userName,
-                    singleLine = true,
-                    onValueChange = setUserName
-                )
-                OutlinedTextField(
-                    modifier = Modifier.fillMaxWidth(),
-                    label = { Text(text = stringResource(id = R.string.password_text)) },
-                    value = userPassword,
-                    singleLine = true,
-                    onValueChange = setUserPassword,
-                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
-                    keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
-                    trailingIcon = {
-                        val image = if (passwordVisible)
-                            Icons.Filled.Visibility
-                        else Icons.Filled.VisibilityOff
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(10.dp),
+                    verticalArrangement = Arrangement.SpaceAround,
 
-                        // Localized description for accessibility services
-                        val description = if (passwordVisible) "Hide password" else "Show password"
+                    ) {
+                    Column(
+                        modifier = Modifier,
+                        verticalArrangement = Arrangement.spacedBy(12.dp)
+                    ) {
+                        Text(
+                            text = stringResource(id = R.string.login_with_simple_account),
+                            style = MaterialTheme.typography.caption
+                        )
+                        OutlinedTextField(
+                            leadingIcon = {
+                                Icon(Icons.Default.Person, contentDescription = null)
+                            },
+                            modifier = Modifier.fillMaxWidth(),
+                            label = { Text(text = stringResource(id = R.string.user_name_text)) },
+                            value = userName,
+                            singleLine = true,
+                            onValueChange = setUserName
+                        )
+                        OutlinedTextField(
+                            modifier = Modifier.fillMaxWidth(),
+                            leadingIcon = {
+                                Icon(Icons.Default.Key, contentDescription = null)
+                            },
+                            label = { Text(text = stringResource(id = R.string.password_text)) },
+                            value = userPassword,
+                            singleLine = true,
+                            onValueChange = setUserPassword,
+                            visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                            trailingIcon = {
+                                val image = if (passwordVisible)
+                                    Icons.Filled.Visibility
+                                else Icons.Filled.VisibilityOff
 
-                        // Toggle button to hide or display password
-                        IconButton(onClick = { setPasswordVisible(!passwordVisible) }) {
-                            Icon(imageVector = image, description)
+                                // Localized description for accessibility services
+                                val description =
+                                    if (passwordVisible) "Hide password" else "Show password"
+
+                                // Toggle button to hide or display password
+                                IconButton(onClick = { setPasswordVisible(!passwordVisible) }) {
+                                    Icon(imageVector = image, description)
+                                }
+                            }
+
+
+                        )
+                        Row(
+                            modifier = Modifier.fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween
+                        ) {
+
+                            TextButton(onClick = { /*TODO*/ }) {
+                                Text(
+                                    text = stringResource(id = R.string.forget_password_text),
+                                    style = MaterialTheme.typography.body2,
+                                    textDecoration = TextDecoration.Underline
+                                )
+                            }
+
+
+                            Button(onClick = { navController?.navigate(NavigationRoute.HomeScreenRoute.route) }) {
+                                Text(text = stringResource(id = R.string.ok))
+                            }
+
                         }
                     }
-
-
-                )
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
-                ) {
-
-                    TextButton(onClick = { /*TODO*/ }) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = 20.dp),
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Divider(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
+                        )
                         Text(
-                            text = stringResource(id = R.string.forget_password_text),
-                            style = MaterialTheme.typography.body2,
-                            textDecoration = TextDecoration.Underline
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(2f),
+
+                            textAlign = TextAlign.Center,
+                            text = stringResource(id = R.string.sign_with_text)
+                        )
+                        Divider(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .weight(1f)
                         )
                     }
 
 
-                    Button(onClick = { navController?.navigate(NavigationRoute.HomeScreenRoute.route) }) {
-                        Text(text = stringResource(id = R.string.ok))
+                    Column(modifier = Modifier, verticalArrangement = Arrangement.spacedBy(12.dp)) {
+                        SignInButton(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = stringResource(id = R.string.sign_with_facebook_text),
+                            icon = painterResource(id = R.drawable.ic_facebook_signin)
+                        ) {
+
+                        }
+                        SignInButton(
+                            modifier = Modifier.fillMaxWidth(),
+                            text = stringResource(id = R.string.sign_with_google_text),
+                            icon = painterResource(id = R.drawable.google_sign_in_btn)
+                        ) {
+
+                        }
                     }
-
-                }
-            }
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(horizontal = 20.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Divider(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                )
-                Text(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(2f),
-
-                    textAlign = TextAlign.Center,
-                    text = stringResource(id = R.string.sign_with_text)
-                )
-                Divider(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(1f)
-                )
-            }
-
-
-            Column(modifier = Modifier, verticalArrangement = Arrangement.spacedBy(12.dp)) {
-                SignInButton(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = stringResource(id = R.string.sign_with_facebook_text),
-                    icon = painterResource(id = R.drawable.ic_facebook_signin)
-                ) {
-
-                }
-                SignInButton(
-                    modifier = Modifier.fillMaxWidth(),
-                    text = stringResource(id = R.string.sign_with_google_text),
-                    icon = painterResource(id = R.drawable.google_sign_in_btn)
-                ) {
-
                 }
             }
         }
